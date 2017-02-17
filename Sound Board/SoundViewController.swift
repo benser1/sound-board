172d038137d1f7110f9d2289ff2192323be83cfc
benser1 <briganenser28@gmail.com>
2017-02-16 19:51:13 -0500

    Added SoundViewController with buttons and text field

---
+++ Sound Board/SoundViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SoundViewController.swift
+//  Sound Board
+//
+//  Created by Brigan Enser on 2/16/17.
+//  Copyright © 2017 Brigan Enser. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+
+    @IBAction func playTapped(_ sender: Any) {
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+    
+}
